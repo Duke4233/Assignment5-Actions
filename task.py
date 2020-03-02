e01e64f9a9a63fa0ed280a5e73e772848e70127c
CarterLu <carterlu@oregonstate.edu>
2020-03-01 20:51:33 -0800

    added circlearea function and correspodning unittests

--- task.py
+++ task.py
@@ -1,2 +1,13 @@
+import math
+
+
+
 def firstrun():
     return "success"
+
+
+def circlearea(radius):
+    if radius < 0:
+        return "Invalid value for radius"
+    area = math.pi * radius**2
+    return area
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import task
+import math
 
 
 class TestCase(unittest.TestCase):
@@ -8,10 +9,33 @@ class TestCase(unittest.TestCase):
         expected = "success"
         self.assertEqual(expected, task.firstrun())
 
-    def test2(self):
-        expected = "failure"
-        self.assertEqual(expected, task.firstrun())
+   # def test2(self):
+    #    expected = "failure"
+     #   self.assertEqual(expected, task.firstrun())
 
+
+    def test__circlearea(self):
+        R1 = 0
+        R2 = 2
+        R3 = 100
+        R4 = -5
+        R5 = 10000
+        # test1
+        expected = 0
+        self.assertEqual(expected,task.circlearea(R1))
+        # test2
+        expected = 4 * math.pi
+        self.assertEqual(expected, task.circlearea(R2))
+        # test3
+        expected = 10000 * math.pi
+        self.assertEqual(expected, task.circlearea(R3))
+        # test4
+        expected = "Invalid value for radius"
+        self.assertEqual(expected, task.circlearea(R4))
+        # test5
+        expected = 10000**2 * math.pi
+        self.assertEqual(expected, task.circlearea(R5))
+        
 
 if __name__ == '__main__':
     unittest.main()
